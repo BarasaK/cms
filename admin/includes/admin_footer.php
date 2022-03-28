57ff6b6dc7bacf5e88a1d5606eb22394aff4314a
BarasaK <nicholas.k.barasa@gmail.com>
2022-03-28 08:37:07 +0300

    WYSIWYG

--- admin/includes/admin_footer.php
+++ admin/includes/admin_footer.php
@@ -12,7 +12,7 @@
     <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.18/dist/summernote.min.js"></script>
 
 
-    <script src="./includes/js/scripts.js"></script>
+    <script src="js/scripts.js"></script>
 
 </body>
 
